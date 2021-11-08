90b8069b9ed26a506bba6f2a883856ded81a63ef
Mohammadreza Aali <80044121+mraali2012@users.noreply.github.com>
2021-11-08 15:40:18 +0000

    Update volField.cpp

--- src/volField.cpp
+++ src/volField.cpp
@@ -58,9 +58,28 @@ void volField<T>::writeHeader(std::string path, std::string filename)
 {
       // TO DO: write back header to a file    
     std::cout<<'{'<<std::endl;
-    std::cout<<"typeName"<<std::endl;
-    std::cout<<'\t'<< typeName.name()<<std::endl;
+    std::cout<<"\tclass\t\t"<<type_<<';'<<std::endl;
+    std::cout<<"\tobject\t\t"<<name_<<";"<<std::endl;
+    std::cout<<"\tobject\t\t"<<obj<<";"<<std::endl;
     std::cout<<"\t;"<<std::endl;
+    std::cout<<"// ************************************************************************* //\n"<< std::endl;
 }
+
+//dimensions
+template <typename T>
+void volField<T>::writeDimensions(std::string path, std::string filename)
+ {
+     if(dimensions_.size() == 7){
+      std::cout<<"dimensions\t[";
+      for (int i = 0; i < 7; i++){
+        std::cout << dimensions_[i];
+        if(i<6) std::cout<< ' ';
+      }
+      std::cout<<"];\n\n"<<std::endl;
+
+    }else{
+      std::cout<<"Cannot write the dimensions, since the data to write is not valid as dimensions!!!"<<std::endl;
+    }
+  }
 
 // Useful functions
