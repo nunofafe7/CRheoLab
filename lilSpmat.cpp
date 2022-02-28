a59e0f61c95e4f536de0b641246b3046e6c5fce6
ricardodpcosta <76709201+ricardodpcosta@users.noreply.github.com>
2022-02-28 16:06:48 +0000

    Final functions

--- lilSpmat.cpp
+++ lilSpmat.cpp
@@ -1,4 +1,5 @@
 #include "lilSpmat.h"
+#include <iostream>
 
 // Constructor
 // Constructs an empty sparse matrix and allocates memory for the rows
@@ -11,8 +12,8 @@ lilSpmat::lilSpmat(unsigned int numRows, unsigned int numCols)
   // Allocate memory for the rows since any row might be accessed at any time
   // Do not do the same for each columns_[i] and values_[i] vector since their size
   // should only increase as new entries are added
-  columns_.reserve(numRows_);
-  values_.reserve(numRows_);
+  columns_.resize(numRows_);
+  values_.resize(numRows_);
 }
 
 // Returns the sparsity of the matrix
@@ -21,29 +22,90 @@ double lilSpmat::sparsity()
   return 0.0;
 }
 
+// Adds a value to position (i,j) if exists, otherwise inserts a new value
+void lilSpmat::addValue(unsigned int i, unsigned int j, double val)
+{
+      for(unsigned int k=0;k<columns_[i].size();k++)
+      {
+            if(columns_[i][k] == j)
+            {
+                  values_[i][k] += val;
+                  return;
+            }
+      }
+      columns_[i].push_back(j);
+      values_[i].push_back(val);
+}
 
-//Conrado - Begin #############################################################
+// Subtracts a value to position (i,j) if exists, otherwise inserts a new value
+void lilSpmat::subValue(unsigned int i, unsigned int j, double val)
+{
+      for(unsigned int k=0;k<columns_[i].size();k++)
+      {
+            if(columns_[i][k] == j)
+            {
+                  values_[i][k] -= val;
+                  return;
+            }
+      }
+      columns_[i].push_back(j);
+      values_[i].push_back(-val);
+}
+
+// Deletes the value in position (i,j) if exists, otherwise does nothing
+void lilSpmat::delValue(unsigned int i, unsigned int j)
+{
+      for(unsigned int k=0;k<columns_[i].size();k++)
+      {
+            if(columns_[i][k] == j)
+            {
+                  columns_[i].erase(columns_[i].begin()+k);
+                  values_[i].erase(values_[i].begin()+k);
+                  return;
+            }
+      }
+}
+
+// Returns the sparse matrix in a dense format as a vector of vectors
+std::vector< std::vector<double> > lilSpmat::dense()
+{
+
+  std::vector< std::vector<double> > denseMatrix(numRows_);
+  std::vector<double> temp(numCols_);
+
+  unsigned int id_column = 0;
+
+  for(unsigned int i=0;i<numRows_;i++)
+  {
+    denseMatrix[i] = temp;
+  }
+
+  for(unsigned int i=0;i<numRows_;i++)
+  {
+    for(unsigned int j=0;j<columns_[i].size();j++)
+    {
+      id_column = columns_[i][j];
+      denseMatrix[i][id_column] = values_[i][j];
+    }
+  }
+
+  return denseMatrix;
+}
 
 // Function that returns the product matrix-vector as a vector
 std::vector<double> lilSpmat::matMul(const std::vector<double> &vecPhi)
 {
   std::vector<double> v(vecPhi.size());
   unsigned int id_column = 0;
-
-  //for(unsigned int i=0;i<spmat.getNumRows();i++)
-  for(unsigned int i=0;i<columns_.size();i++)
+  for(unsigned int i=0;i<numRows_;i++)
   {
     v[i] = 0.0;
-    
     for(unsigned int j=0;j<columns_[i].size();j++)
     {
-
       id_column = columns_[i][j];
       v[i] += values_[i][j] * vecPhi[id_column];
-
     }
   }
-
   return v;
 }
 
@@ -77,23 +139,17 @@ std::vector<double> lilSpmat::matMul(const std::vector<double> &vecPhi)
   return v;
 }*/
 
+// Returns the product (row-of-matrix)-vector for a specific row of the matrix as a double
 double lilSpmat::vecMul(const unsigned int i, const std::vector<double> &vecPhi)
 {
-
   double sumProdRow = 0.0;
   unsigned int id_column = 0;
-
   for(unsigned int j=0;j<columns_[i].size();j++)
   {
-
     id_column = columns_[i][j];
-
     sumProdRow += values_[i][j] * vecPhi[id_column];
-
   }
-
   return sumProdRow;
 }
-
 
 //Conrado - End #############################################################
--- main.cpp
+++ main.cpp
@@ -5,53 +5,10 @@
 
 int main(int argc, char const *argv[]) {
 
-      // Declare and initialize a sparse matrix
-      lilSpmat spmat = lilSpmat(4,4);
+      std::cout << "#############################################################" << std::endl;
+      std::cout << "Test sparse matrix" << std::endl;
+      std::cout << "#############################################################" << std::endl;
 
-      // Display the number of elements in each row
-      for(unsigned int i=0;i<spmat.getNumRows();i++)
-      {
-            std::cout << spmat.columns_[i].size() << std::endl;
-      }
-
-      std::cout << " " << std::endl;
-
-      // Display the number of elements in each row
-      // Should provide the same as above since both columns_ and values_ should
-      // have the same size
-      for(unsigned int i=0;i<spmat.getNumRows();i++)
-      {
-            std::cout << spmat.values_[i].size() << std::endl;
-      }
-
-      std::cout << " " << std::endl;
-
-      // Display the columns in each row
-      std::vector<unsigned int>::iterator iit;
-      for(unsigned int i=0;i<spmat.getNumRows();i++)
-      {
-            for(iit = spmat.columns_[i].begin(); iit != spmat.columns_[i].end(); ++iit)
-            {
-                  std::cout << *iit;
-            }
-            std::cout << std::endl;
-      }
-
-      std::cout << " " << std::endl;
-
-      // Display the columns in each row
-      std::vector<double>::iterator dit;
-      for(unsigned int i=0;i<spmat.getNumRows();i++)
-      {
-            for(dit = spmat.values_[i].begin(); dit != spmat.values_[i].end(); ++dit)
-            {
-                  std::cout << *dit;
-            }
-            std::cout << std::endl;
-      }
-
-
-      //Conrado - Begin #############################################################
       std::vector<std::vector<double>> spMatTest //Sparse Matrix for Test
       {
         {1.0, 0.0, 2.0, 0.0},
@@ -59,8 +16,6 @@ int main(int argc, char const *argv[]) {
         {0.0, 4.0, 0.0, 5.0},
         {0.0, 0.0, 0.0, 6.0}
       };
-
-      std::cout << "Sparse matrix:" << std::endl;
 
       for (int i = 0; i < spMatTest.size(); i++)
       {
@@ -71,24 +26,15 @@ int main(int argc, char const *argv[]) {
             std::cout << std::endl;
       }
 
-      spmat.columns_=
+      //#############################################################
+
+      /*spmat.columns_=
       {
             {0, 2},
             {1},
             {1, 3},
             {3}
       };
-      
-      std::cout << "Columns indexes:" << std::endl;
-
-      for (int i = 0; i < spmat.columns_.size(); i++)
-      {
-            for (int j = 0; j < spmat.columns_[i].size(); j++)
-            {
-                  std::cout << spmat.columns_[i][j] << " ";
-            }   
-            std::cout << std::endl;
-      }
 
       spmat.values_=
       {
@@ -96,7 +42,43 @@ int main(int argc, char const *argv[]) {
             {3.0},
             {4.0, 5.0},
             {6.0}
-      };
+      };*/
+
+      //#############################################################
+
+      std::cout << "#############################################################" << std::endl;
+      std::cout << "Test sparse matrix construction" << std::endl;
+      std::cout << "#############################################################" << std::endl;
+
+      // Declare and initialize a sparse matrix
+      lilSpmat spmat = lilSpmat(4,4);
+
+      // add values
+      spmat.addValue(0,0,1.0);
+      spmat.addValue(0,2,2.0);
+      spmat.addValue(1,1,3.0);
+      spmat.addValue(2,1,4.0);
+      spmat.addValue(2,3,5.0);
+      spmat.addValue(3,3,6.0);
+
+      // add some garbage values
+      spmat.addValue(2,2,100.0);
+      spmat.addValue(3,2,100.0);
+
+      // delete garbage values
+      spmat.delValue(2,2);
+      spmat.delValue(3,2);
+
+      std::cout << "Columns indexes:" << std::endl;
+
+      for (int i = 0; i < spmat.columns_.size(); i++)
+      {
+            for (int j = 0; j < spmat.columns_[i].size(); j++)
+            {
+                  std::cout << spmat.columns_[i][j] << " ";
+            }
+            std::cout << std::endl;
+      }
 
       std::cout << "Columns values:" << std::endl;
 
@@ -105,38 +87,59 @@ int main(int argc, char const *argv[]) {
             for (int j = 0; j < spmat.values_[i].size(); j++)
             {
                   std::cout << spmat.values_[i][j] << " ";
-            }   
+            }
             std::cout << std::endl;
       }
 
-      std::vector<double> vecPhi{ 1.0, 2.0, 3.0, 4.0 }; //std::vector< double > vecPhi_(spmat.getNumRows()); //vecPhi_(numRows_);
+      std::cout << "#############################################################" << std::endl;
+      std::cout << "Test sparse matrix to dense format" << std::endl;
+      std::cout << "#############################################################" << std::endl;
+
+      // Vector of vectors to store the sparse matrix in a dense format
+      std::vector< std::vector<double> > denseMatrix;
+
+      // Returns the sparse matrix in a dense format as a vector of vectors
+      denseMatrix = spmat.dense();
+
+      std::cout << "Dense matrix:" << std::endl;
+
+      for (int i = 0; i < denseMatrix.size(); i++)
+      {
+            for (int j = 0; j < denseMatrix[i].size(); j++)
+            {
+                  std::cout << denseMatrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+      }
+
+      std::cout << "#############################################################" << std::endl;
+      std::cout << "Test sparse matrix multiplication" << std::endl;
+      std::cout << "#############################################################" << std::endl;
+
+      std::vector<double> vecPhi{ 1.0, 2.0, 3.0, 4.0};
 
       std::cout << "Phi vector for multiplication:" << std::endl;
 
       for (double valuePhi : vecPhi)
-            std::cout << valuePhi << " ";
-      std::cout << std::endl;
+            std::cout << valuePhi << std::endl;
 
       std::vector<double> v(4); //Vector resulting from the matrix-vector product
 
       //Call the function that returns the product matrix-vector as a vector
       v = spmat.matMul(vecPhi);
-      
+
       std::cout << "Vector resulting from the matrix-vector product:" << std::endl;
 
       for (double valueV : v)
-            std::cout << valueV << " ";
-      std::cout << std::endl;
+            std::cout << valueV << std::endl;
 
       unsigned int rowMatVecProd = 2;
 
       //Call the function that calculates the product (row-of-matrix)-vector for a specific row of the matrix
       double ProdRowMatVec = spmat.vecMul(rowMatVecProd, vecPhi);
 
-      std::cout << "Double resulting from the (row-of-matrix)-vector product:" << std::endl;    
+      std::cout << "Double resulting from the (row-of-matrix)-vector product:" << std::endl;
       std::cout << "For row: " << rowMatVecProd << ", Product: " << ProdRowMatVec << std::endl;
-      //Conrado - End #############################################################
 
       return 0;
 }
-
