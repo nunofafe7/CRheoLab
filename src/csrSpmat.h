14f08681ac2a959e79d3f1cb519e4c36dd47e67b
mnobrega <mnobrega@dep.uminho.pt>
2022-03-24 13:03:45 +0000

    Merge pull request #45 from Computational-Rheology/Group1Task3
    Group1 task3

---
+++ src/csrSpmat.h
@@ -0,0 +1,57 @@
+#ifndef CSRSPMAT_H
+#define CSRSPMAT_H
+
+#include "spmat.h"
+#include <vector>
+
+// Class to implement a list of lists for a sparse matrix
+// The std::vector structure is used to implement the list
+
+class csrSpmat : public spmat {
+
+public: // change to private later
+
+
+  std::vector<double> values_;
+
+
+  std::vector<unsigned int> columns_;
+
+
+  std::vector<unsigned int> row_ptr_;
+
+public:
+
+  // Constructor
+  csrSpmat(unsigned int numRows, unsigned int numCols, fvMesh mesh);
+
+  // Destructor
+  virtual ~csrSpmat(){};
+
+  // Returns the sparsity of the matrix
+  double sparsity();
+
+  // Adds a value to position (i,j) if exists, otherwise inserts a new value
+  void addValue(unsigned int i, unsigned int j, double val);
+
+  // Subtracts a value to position (i,j) if exists, otherwise inserts a new value with oposite sign
+  void subValue(unsigned int i, unsigned int j, double val);
+
+  // Deletes the value in position (i,j) if exists, otherwise does nothing
+  void delValue(unsigned int i, unsigned int j);
+
+  // Returns the value in position (i,j) if exists, otherwise returns 0
+  double getValue(unsigned int i, unsigned int j);
+
+  // Returns the sparse matrix in a dense format as a vector of vectors
+  std::vector< std::vector<double> > dense();
+
+  // Returns the product matrix-vector as a vector
+  std::vector<double> matMul(const std::vector<double> &vecPhi);
+
+  // Returns the product (row-of-matrix)-vector for a specific row of the matrix as a double
+  double vecMul(const unsigned int i, const std::vector<double> &vecPhi);
+
+};
+
+#endif // CSRSPMAT_H
---
+++ src/lilSpmat.cpp
@@ -0,0 +1,135 @@
+#include "lilSpmat.h"
+#include <iostream>
+
+// Constructor
+// Constructs an empty sparse matrix and allocates memory for the rows
+lilSpmat::lilSpmat(unsigned int numRows, unsigned int numCols)
+{
+  // Store number of rows and columns
+  numRows_ = numRows;
+  numCols_ = numCols;
+
+  // Allocate memory for the rows since any row might be accessed at any time
+  // Do not do the same for each columns_[i] and values_[i] vector since their size
+  // should only increase as new entries are added
+  columns_.resize(numRows_);
+  values_.resize(numRows_);
+}
+
+// Returns the sparsity of the matrix
+double lilSpmat::sparsity()
+{
+  unsigned int nz = 0;
+  for(unsigned int i=0;i< numRows_;i++) {
+     nz += columns_[i].size();
+  }
+  return (1.0 - ((double)nz / ((double)(numRows_ * numCols_))));
+}
+
+// Adds a value to position (i,j) if exists, otherwise inserts a new value
+void lilSpmat::addValue(unsigned int i, unsigned int j, double val)
+{
+  for(unsigned int k=0;k<columns_[i].size();k++)
+  {
+    if(columns_[i][k] == j)
+    {
+      values_[i][k] += val;
+      return;
+    }
+  }
+  columns_[i].push_back(j);
+  values_[i].push_back(val);
+}
+
+// Subtracts a value to position (i,j) if exists, otherwise inserts a new value
+void lilSpmat::subValue(unsigned int i, unsigned int j, double val)
+{
+  for(unsigned int k=0;k<columns_[i].size();k++)
+  {
+    if(columns_[i][k] == j)
+    {
+      values_[i][k] -= val;
+      return;
+    }
+  }
+  columns_[i].push_back(j);
+  values_[i].push_back(-val);
+}
+
+// Deletes the value in position (i,j) if exists, otherwise does nothing
+void lilSpmat::delValue(unsigned int i, unsigned int j)
+{
+  for(unsigned int k=0;k<columns_[i].size();k++)
+  {
+    if(columns_[i][k] == j)
+    {
+      columns_[i].erase(columns_[i].begin()+k);
+      values_[i].erase(values_[i].begin()+k);
+      return;
+    }
+  }
+}
+
+// Returns the value in position (i,j) if exists, otherwise returns 0
+double lilSpmat::getValue(unsigned int i, unsigned int j);
+{
+  for(unsigned int k=0;k<columns_[i].size();k++)
+  {
+    if(columns_[i][k] == j)
+    {
+      return values_[i][k];
+    }
+  }
+  return 0.0;
+}
+
+// Returns the sparse matrix in a dense format as a vector of vectors
+std::vector< std::vector<double> > lilSpmat::dense()
+{
+  std::vector< std::vector<double> > denseMatrix(numRows_);
+  std::vector<double> temp(numCols_);
+  unsigned int id_column = 0;
+  for(unsigned int i=0;i<numRows_;i++)
+  {
+    denseMatrix[i] = temp;
+  }
+  for(unsigned int i=0;i<numRows_;i++)
+  {
+    for(unsigned int j=0;j<columns_[i].size();j++)
+    {
+      id_column = columns_[i][j];
+      denseMatrix[i][id_column] = values_[i][j];
+    }
+  }
+  return denseMatrix;
+}
+
+// Function that returns the product matrix-vector as a vector
+std::vector<double> lilSpmat::matMul(const std::vector<double> &vecPhi)
+{
+  std::vector<double> v(vecPhi.size());
+  unsigned int id_column = 0;
+  for(unsigned int i=0;i<numRows_;i++)
+  {
+    v[i] = 0.0;
+    for(unsigned int j=0;j<columns_[i].size();j++)
+    {
+      id_column = columns_[i][j];
+      v[i] += values_[i][j] * vecPhi[id_column];
+    }
+  }
+  return v;
+}
+
+// Returns the product (row-of-matrix)-vector for a specific row of the matrix as a double
+double lilSpmat::vecMul(const unsigned int i, const std::vector<double> &vecPhi)
+{
+  double sumProdRow = 0.0;
+  unsigned int id_column = 0;
+  for(unsigned int j=0;j<columns_[i].size();j++)
+  {
+    id_column = columns_[i][j];
+    sumProdRow += values_[i][j] * vecPhi[id_column];
+  }
+  return sumProdRow;
+}
