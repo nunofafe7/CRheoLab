871d52e4bb90e6b5f6205c25557ef1a55d5c7d77
ricardodpcosta <76709201+ricardodpcosta@users.noreply.github.com>
2022-03-28 19:45:18 +0100

    Update.

--- src/spmat.h
+++ src/spmat.h
@@ -33,27 +33,25 @@ public:
   }
 
   // Adds a value to position (i,j) if exists, otherwise inserts a new value
-  void addValue(unsigned int i, unsigned int j, double val);
+  virtual void addValue(unsigned int i, unsigned int j, double val) = 0;
 
   // Subtracts a value to position (i,j) if exists, otherwise inserts a new value with oposite sign
-  void subValue(unsigned int i, unsigned int j, double val);
+  virtual void subValue(unsigned int i, unsigned int j, double val) = 0;
 
   // Deletes the value in position (i,j) if exists, otherwise does nothing
-  void delValue(unsigned int i, unsigned int j);
+  virtual void delValue(unsigned int i, unsigned int j) = 0;
 
   // Returns the value in position (i,j) if exists, otherwise returns 0
-  double getValue(unsigned int i, unsigned int j);
+  virtual double getValue(unsigned int i, unsigned int j) = 0;
 
   // Returns the sparse matrix in a dense format as a vector of vectors
-  std::vector< std::vector<double> > dense();
+  virtual std::vector< std::vector<double> > dense() = 0;
 
   // Returns the product matrix-vector as a vector
-  //std::vector<double> matMul(std::vector<double> v);
-  std::vector<double> matMul(std::vector<double> &v, const std::vector<double> &vecPhi);
+  virtual std::vector<double> matMul(const std::vector<double> &vecPhi) = 0;
 
   // Returns the product (row-of-matrix)-vector for a specific row of the matrix as a double
-  //double vecMul(unsigned int i, std::vector<double> v);
-  double vecMul(const unsigned int i, const std::vector<double> &vecPhi);
+  virtual double vecMul(const unsigned int i, const std::vector<double> &vecPhi) = 0;
 
 };
 
